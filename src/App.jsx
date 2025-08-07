8c0f595eb2d3934c76facb3305b309970ac39677
morgantstewart <morgan@paysigil.com>
2025-08-07 17:31:42 -0400

    creating useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 // src/App.jsx
 
+import { useState, useEffect } from 'react';
 import './App.css';
 
-const App = () => {
+function App() {
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  const [user, setUser] = useState({
+    firstName: 'Morgan',
+    lastName: 'Stewart',
+    hasPets: true,
+    age: 34,
+  });
+
+  useEffect(() => {
+    console.log('Our isDarkMode state value is:', isDarkMode);
+    console.log('User state:', user);
+  }, [isDarkMode, user]);
+
   return (
-    <h1>Hello world!</h1>
+    <div>
+      <h1>
+        Hello, my name is {user.firstName} {user.lastName}, I am {user.age} years old,
+        and I {user.hasPets ? 'have' : "don't have"} pets.
+      </h1>
+    </div>
   );
-};
+}
 
 export default App;
