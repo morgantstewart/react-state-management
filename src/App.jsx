97066515c421bef21c66cd3fee6348de095c6237
morgantstewart <morgan@paysigil.com>
2025-08-07 22:33:16 -0400

    final app component for refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,26 @@
-// src/App.jsx
-import { useState, useEffect } from 'react';
+// src/App.js
+
+import { useState } from 'react';
 import './App.css';
 
-function App() {
-  const [isDarkMode, setIsDarkMode] = useState(true);
-  const [user, setUser] = useState({
-    firstName: 'Morgan',
-    lastName: 'Stewart',
-    hasPets: true,
-    age: 34,
-  });
+const App = () => {
+  const [mode, setMode] = useState('light');
 
-  const handleDarkMode = () => {
-    setIsDarkMode(true);
-    console.log('Dark Mode!');
+  const handleMode = (modeValue) => {
+    setMode(modeValue);
   };
-
-  const handleLightMode = () => {
-    setIsDarkMode(false);
-    console.log('Light Mode!');
-  };
-
-  useEffect(() => {
-    console.log('Our isDarkMode state value is:', isDarkMode);
-    console.log('User state:', user);
-  }, [isDarkMode, user]);
 
   return (
     <>
-      <div>
-        <h1>
-          Hello, my name is {user.firstName} {user.lastName}, I am {user.age} years old,
-          and I {user.hasPets ? 'have' : "don't have"} pets.
-        </h1>
+      <div className={mode}>
+        <h1>Hello world!</h1>
       </div>
-
-      <div className={isDarkMode ? 'dark' : 'light'}>
-    
-      </div>
-
       <div>
-        <button onClick={handleDarkMode}>Dark Mode</button>
-        <button onClick={handleLightMode}>Light Mode</button>
+        <button onClick={() => handleMode('dark')}>Dark Mode</button>
+        <button onClick={() => handleMode('light')}>Light Mode</button>
       </div>
     </>
   );
-}
+};
 
 export default App;
