bf8c6bf40fadd2c696e7446c0b197db1ba4c2421
morgantstewart <morgan@paysigil.com>
2025-08-07 22:22:10 -0400

    commit for appjsx refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,9 @@
-
+// src/App.jsx
 import { useState, useEffect } from 'react';
 import './App.css';
-// src/App.jsx
 
-const App = () => {
-  const [mode, setMode] = useState('light');
-
-};
-
-const handleDarkMode = () => {
-
-  setIsDarkMode(true);
-  console.log('Dark Mode!');
-};
-   const handleLightMode = () => {
-  setIsDarkMode(false);
-  console.log('Light Mode!');
-};
-
-
+function App() {
+  const [isDarkMode, setIsDarkMode] = useState(true);
   const [user, setUser] = useState({
     firstName: 'Morgan',
     lastName: 'Stewart',
@@ -26,23 +11,34 @@ const handleDarkMode = () => {
     age: 34,
   });
 
+  const handleDarkMode = () => {
+    setIsDarkMode(true);
+    console.log('Dark Mode!');
+  };
+
+  const handleLightMode = () => {
+    setIsDarkMode(false);
+    console.log('Light Mode!');
+  };
+
   useEffect(() => {
     console.log('Our isDarkMode state value is:', isDarkMode);
     console.log('User state:', user);
   }, [isDarkMode, user]);
 
-  // return (
-  //   <div>
-  //     <h1>
-  //       Hello, my name is {user.firstName} {user.lastName}, I am {user.age} years old,
-  //       and I {user.hasPets ? 'have' : "don't have"} pets.
-  //     </h1>
-  //   </div>
-    return (
+  return (
     <>
-      <div className={isDarkMode ? 'dark' : 'light'}>
-        <h1>Hello world!</h1>
+      <div>
+        <h1>
+          Hello, my name is {user.firstName} {user.lastName}, I am {user.age} years old,
+          and I {user.hasPets ? 'have' : "don't have"} pets.
+        </h1>
       </div>
+
+      <div className={isDarkMode ? 'dark' : 'light'}>
+    
+      </div>
+
       <div>
         <button onClick={handleDarkMode}>Dark Mode</button>
         <button onClick={handleLightMode}>Light Mode</button>
